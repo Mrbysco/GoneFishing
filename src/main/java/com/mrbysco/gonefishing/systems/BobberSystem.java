5118aee8b3306dfcb8072edb15a69969e5ededb2
Mrbysco <Mrbysco@users.noreply.github.com>
2026-01-17 16:04:01 +0100

    Re-format code

--- src/main/java/com/mrbysco/gonefishing/systems/BobberSystem.java
+++ src/main/java/com/mrbysco/gonefishing/systems/BobberSystem.java
@@ -11,67 +11,66 @@ import com.hypixel.hytale.server.core.entity.AnimationUtils;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.mrbysco.gonefishing.GoneFishingPlugin;
 import com.mrbysco.gonefishing.component.BobberComponent;
-import com.mrbysco.gonefishing.util.FishHelper;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 import javax.annotation.Nonnull;
 
 public class BobberSystem extends EntityTickingSystem<EntityStore> {
 
-    @Override
-    public void tick(float dt, int index, @Nonnull ArchetypeChunk<EntityStore> archetypeChunk,
-                     @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
+	@Override
+	public void tick(float dt, int index, @Nonnull ArchetypeChunk<EntityStore> archetypeChunk,
+	                 @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
 
-        Ref<EntityStore> bobberRef = archetypeChunk.getReferenceTo(index);
-        BobberComponent component = store.getComponent(bobberRef, BobberComponent.getComponentType());
-        if (component != null) {
-            int newAge = component.getBobberAge() + 1;
-            if (newAge < 5) {
-                AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Spawn", true, store);
-            }
-            component.setBobberAge(newAge);
+		Ref<EntityStore> bobberRef = archetypeChunk.getReferenceTo(index);
+		BobberComponent component = store.getComponent(bobberRef, BobberComponent.getComponentType());
+		if (component != null) {
+			int newAge = component.getBobberAge() + 1;
+			if (newAge < 5) {
+				AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Spawn", true, store);
+			}
+			component.setBobberAge(newAge);
 
-            if (newAge < 50) {
-                return; // Wait until bobber is fully spawned
-            }
+			if (newAge < 50) {
+				return; // Wait until bobber is fully spawned
+			}
 
-            // Check if a time until catch is set
-            if (component.canCatchFish()) {
-                int newCatchTimer = component.getCatchTimer() - 1;
-                if (newCatchTimer > 0) {
-                    component.setCatchTimer(newCatchTimer);
-                } else {
-                    // Reset catch state after timer expires
-                    component.setCanCatch(false);
-                    resetTimeUntilCatch(component);
-                    AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Idle", true, store);
-                }
-            } else {
-                if (component.getTimeUntilCatch() != -1) {
-                    if (component.getTimeUntilCatch() <= 0) {
-                        component.setCanCatch(true);
-                        AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Catch", true, store);
-                    } else {
-                        int newTimeUntilCatch = component.getTimeUntilCatch() - 1;
-                        component.setTimeUntilCatch(newTimeUntilCatch);
-                    }
-                } else {
-                    resetTimeUntilCatch(component);
-                    AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Idle", true, store);
-                }
-            }
-        }
-    }
+			// Check if a time until catch is set
+			if (component.canCatchFish()) {
+				int newCatchTimer = component.getCatchTimer() - 1;
+				if (newCatchTimer > 0) {
+					component.setCatchTimer(newCatchTimer);
+				} else {
+					// Reset catch state after timer expires
+					component.setCanCatch(false);
+					resetTimeUntilCatch(component);
+					AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Idle", true, store);
+				}
+			} else {
+				if (component.getTimeUntilCatch() != -1) {
+					if (component.getTimeUntilCatch() <= 0) {
+						component.setCanCatch(true);
+						AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Catch", true, store);
+					} else {
+						int newTimeUntilCatch = component.getTimeUntilCatch() - 1;
+						component.setTimeUntilCatch(newTimeUntilCatch);
+					}
+				} else {
+					resetTimeUntilCatch(component);
+					AnimationUtils.playAnimation(bobberRef, AnimationSlot.Status, "Idle", true, store);
+				}
+			}
+		}
+	}
 
-    private void resetTimeUntilCatch(BobberComponent component) {
-        // Set a random time until catch
-        int randomTime = 100;
-        component.setTimeUntilCatch(randomTime);
-    }
+	private void resetTimeUntilCatch(BobberComponent component) {
+		// Set a random time until catch
+		int randomTime = 100;
+		component.setTimeUntilCatch(randomTime);
+	}
 
-    @NullableDecl
-    @Override
-    public Query<EntityStore> getQuery() {
-        return GoneFishingPlugin.bobberComponent;
-    }
+	@NullableDecl
+	@Override
+	public Query<EntityStore> getQuery() {
+		return GoneFishingPlugin.bobberComponent;
+	}
 }
